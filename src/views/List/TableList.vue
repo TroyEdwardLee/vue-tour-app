fbd843681f933b33c5965d05a168e1ce9cda020f
TroyEdwardLee <troy.edward.lee@outlook.com>
2018-12-13 16:46:02 +0800

    [FEATURE] add formatter
    add a filter for table list desc.

--- src/views/List/TableList.vue
+++ src/views/List/TableList.vue
@@ -5,18 +5,24 @@
     border
     style="width: 100%">
     <el-table-column
+      prop="sha"
+      label="版本号"
+      width="180">
+    </el-table-column>
+    <el-table-column
+      prop="commit.author.name"
+      label="提交者"
+      width="180">
+    </el-table-column>
+    <el-table-column
       prop="commit.author.date"
       label="日期"
       width="180">
     </el-table-column>
     <el-table-column
-      prop="commit.author.name"
-      label="姓名"
-      width="180">
-    </el-table-column>
-    <el-table-column
-      prop="commit.author.email"
-      label="地址">
+      prop="commit.message"
+      :formatter="truncate"
+      label="描述">
     </el-table-column>
   </el-table>
 </template>
@@ -33,6 +39,11 @@ export default {
   computed: {
     ...mapGetters(['tableData'])
   },
-  watch: {}
+  filters: {
+    truncate: function (val) {
+      var newline = val.indexOf('\n')
+      return newline > 0 ? val.slice(0, newline) : val
+    }
+  }
 }
 </script>
