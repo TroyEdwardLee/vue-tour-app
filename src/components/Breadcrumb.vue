d7dd4b67802fdaff0edbc7244b07d378dfd0cdd5
TroyEdwardLee <troy.edward.lee@outlook.com>
2018-12-20 15:04:29 +0800

    [FEATURE] add transition for breadcrum

--- src/components/Breadcrumb.vue
+++ src/components/Breadcrumb.vue
@@ -1,11 +1,11 @@
 <template>
   <el-breadcrumb separator-class="el-icon-arrow-right">
     <el-breadcrumb-item :to="{ path: '/' }">{{$t('pageElText.homeTxt')}}</el-breadcrumb-item>
-      <el-breadcrumb-item v-for="breadCrumb in breadCrumbs" :key="breadCrumb.id">
-        {{ breadCrumb.name }}
-      </el-breadcrumb-item>
-    <!-- <transition-group name="breadcrumb">
-    </transition-group> -->
+      <transition name="component-fade" mode="out-in">
+        <el-breadcrumb-item v-for="breadCrumb in breadCrumbs" :key="breadCrumb.id">
+          {{ breadCrumb.name }}
+        </el-breadcrumb-item>
+      </transition>
   </el-breadcrumb>
 </template>
 
@@ -22,9 +22,10 @@ export default {
 }
 </script>
 <style scoped>
-  .el-breadcrumb__inner {
-    -webkit-transition: color .2s cubic-bezier(.645,.045,.355,1);
-    transition: color .2s cubic-bezier(.645,.045,.355,1);
-    color: #303133;
+  .component-fade-enter-active, .component-fade-leave-active {
+    transition: opacity .4s ease;
+  }
+  .component-fade-enter, .component-fade-leave-to {
+    opacity: 0;
   }
 </style>
