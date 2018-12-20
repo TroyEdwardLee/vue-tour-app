3ea09787de5193372fdc1ab0510ff92189a10dd1
TroyEdwardLee <troy.edward.lee@outlook.com>
2018-12-20 14:57:25 +0800

    [INTRENAL] modify host binding

--- src/components/Breadcrumb.vue
+++ src/components/Breadcrumb.vue
@@ -1,11 +1,11 @@
 <template>
   <el-breadcrumb separator-class="el-icon-arrow-right">
     <el-breadcrumb-item :to="{ path: '/' }">{{$t('pageElText.homeTxt')}}</el-breadcrumb-item>
-    <el-breadcrumb-item v-for="breadCrumb in breadCrumbs" :key="breadCrumb.id">
-      {{ breadCrumb.name }}
-    </el-breadcrumb-item>
-    <!-- <el-breadcrumb-item>项目</el-breadcrumb-item>
-    <el-breadcrumb-item>页面一</el-breadcrumb-item> -->
+      <el-breadcrumb-item v-for="breadCrumb in breadCrumbs" :key="breadCrumb.id">
+        {{ breadCrumb.name }}
+      </el-breadcrumb-item>
+    <!-- <transition-group name="breadcrumb">
+    </transition-group> -->
   </el-breadcrumb>
 </template>
 
@@ -21,3 +21,10 @@ export default {
   }
 }
 </script>
+<style scoped>
+  .el-breadcrumb__inner {
+    -webkit-transition: color .2s cubic-bezier(.645,.045,.355,1);
+    transition: color .2s cubic-bezier(.645,.045,.355,1);
+    color: #303133;
+  }
+</style>
